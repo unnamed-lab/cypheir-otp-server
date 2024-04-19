f22e11a155a829b1d2932c8e3a0e351722575624
Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
2024-04-19 22:20:43 +0100

    alphanumeric otp generator

--- src/utils/generators.ts
+++ src/utils/generators.ts
@@ -1,20 +1,49 @@
 const { createHmac } = require("crypto");
 const { hmac } = require("../utils/hash");
 
-const HOTP = (key: string, counter: number, digits: number = 6): string => {
-  const secretKey = Buffer.from(key);
-  const counterBuffer = Buffer.alloc(8);
-  counterBuffer.writeBigInt64BE(BigInt(counter), 0);
-
-  const hmacDigest = hmac(secretKey, counterBuffer);
-
-  const offset = hmacDigest[hmacDigest.length - 1] & 0x0f;
-  const binaryCode = hmacDigest.slice(offset, offset + 4);
-  const dynamicValue = binaryCode.readUInt32BE() & 0x7fffffff;
-
-  const otp = dynamicValue % 10 ** digits;
-
-  return otp.toString().padStart(digits, "0");
+type THTOPOption = {
+  type: "numeric" | "alphanumeric";
+  offset: number;
 };
+
+const HOTP = (
+  key: string,
+  digits: number = 6,
+  option: THTOPOption = {
+    type: "numeric",
+    offset: 0,
+  }
+): string => {
+    if (digits <= 12 ) {
+
+        const { type, offset } = option;
+        const rand = Math.floor(Math.random() * (20 + offset) + 1);
+      
+        const secretKey = Buffer.from(key);
+        const counterBuffer = Buffer.alloc(8);
+        counterBuffer.writeBigInt64BE(BigInt(rand), 0);
+        const hmacDigest = hmac(secretKey, counterBuffer);
+      
+        const displacement = hmacDigest[hmacDigest.length - 1] & 0x0f;
+        const binaryCode = hmacDigest.slice(displacement, displacement + 4);
+        const dynamicValue = binaryCode.readUInt32BE() & 0x7fffffff;
+      
+        if (type === "numeric") {
+          const otp = dynamicValue % 10 ** digits;
+          return otp.toString().padStart(digits, "0");
+        }
+      
+        const largeDynamicValue = BigInt(dynamicValue ** rand);
+        const base36OTP = BigInt(largeDynamicValue).toString(36).toLowerCase();
+      
+        return base36OTP.slice(rand, digits + rand);
+    }
+
+    throw new Error("The digit input is greate than the maximum (12).")
+};
+
+const CodeFuse = () => {
+
+}
 
 export = { HOTP };
--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,8 +1,14 @@
-const { createHmac } = require("crypto");
+const { createHmac, scryptSync } = require("crypto");
 
 function hmac(key: string, code: string): string {
   const hash: string = createHmac("sha256", key).update(code).digest();
   return hash;
 }
 
-export = { hmac };
+function salt(value: string, key: string) {
+  const salt = Number(key).toString(16);
+  const hashValue = scryptSync(value, salt, 64).toString("hex");
+  return hashValue;
+}
+
+export = { hmac, salt };
