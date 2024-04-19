cb547c1a847156362cc71e9ba6c8ba0c75417e1c
Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
2024-04-19 23:18:10 +0100

    credential keys (client-server) validator

--- src/routes/codes.ts
+++ src/routes/codes.ts
@@ -1,7 +1,7 @@
 const express = require("express");
 const codeRouter = express.Router();
 const { HOTP, CodeFuse } = require("../utils/generators");
-const { salt } = require("../utils/hash");
+const { credValidator } = require("../utils/validator");
 
 codeRouter.get("/", (res: any, req: any): void => {
   req.status(200).send("Response sent!");
@@ -9,22 +9,24 @@ codeRouter.get("/", (res: any, req: any): void => {
 
 codeRouter.post("/create", (res: any, req: any): void => {
   const OTP = HOTP("unnamed", 6, { type: "alphanumeric" });
+  const key1 = CodeFuse({
+    value: "unnamed",
+    key: OTP,
+    client: "127.0.0.1",
+  });
+
+  const key2 = CodeFuse({
+    value: "unnamed",
+    key: OTP,
+    client: "127.0.0.1",
+  });
+
+  const validator = credValidator(key1, key2, () => {
+    return "Callback function works!";
+  });
 
   console.log(`OTP code: ${OTP}`);
-  console.log(
-    CodeFuse({
-      value: "unnamed",
-      key: OTP,
-      client: "127.0.0.1",
-    })
-  );
-  console.log(
-    CodeFuse({
-      value: "unname",
-      key: OTP,
-      client: "127.0.0.1",
-    })
-  );
+  console.log(validator);
 
   req.status(200).send(`OTP code: ${OTP}`);
 });
