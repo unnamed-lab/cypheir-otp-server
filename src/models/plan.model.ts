7bc0fee591688fec59fc3d856e19a061bcf241f1
Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
2024-04-20 18:40:03 +0100

    added plan model

---
+++ src/models/plan.model.ts
@@ -0,0 +1,15 @@
+import { Schema, model } from "mongoose";
+
+interface IPlan {
+  name: string;
+  price: number;
+}
+
+const planSchema = new Schema<IPlan>({
+  name: { type: String, required: true },
+  price: { type: Number, required: true, default: 0, max: 1000, min: 0 },
+});
+
+const Plan = model<IPlan>("Plan", planSchema);
+
+export = { Plan };
