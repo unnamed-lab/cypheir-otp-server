d2c480defe194ce38a137fa0684e1ac1687eb411
Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
2024-04-20 19:37:40 +0100

    Setup MongoDB connector

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,33 @@
-const express = require("express");
+import mongoose from "mongoose";
+import express from "express";
+
+require("dotenv").config();
 
 const app = express();
 const PORT = 3000;
+const API_URI = process.env.MONGODB_URI;
 
+app.use("/api/", require("./routes/codes"));
 
-app.use("/api/", require('./routes/codes'))
+app.listen(PORT, async (): Promise<void> => {
+  console.log(`The server is running on ${PORT}`);
 
-app.listen(PORT, (): void => {
-    console.log(`The server is running on ${PORT}`);
-})
+  if (!API_URI) console.log("Input your MongoDB URI key");
+  else {
+    const connectDB = async () => {
+      return await mongoose.connect(API_URI);
+    };
+    const isConnected = await connectDB();
 
-export = {}
+    try {
+      if (isConnected) {
+        return console.log("MongoDB connected!");
+      }
+      return console.log("MongoDB couldn't connect :(")
+    } catch (error) {
+      console.error(error);
+    }
+  }
+});
+
+export = {};
